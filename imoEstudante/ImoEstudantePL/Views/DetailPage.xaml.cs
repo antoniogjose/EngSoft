5a29a5dbc732b101a57b640da93f0578309d6951
antoniogjose <antonio.g.joe@gmail.com>
2016-10-09 23:42:33 +0100

    Template10
    Testes com a extensão Template10, para criação de aplicações para Windwos 10. Videos de apoio :
    https://mva.microsoft.com/en-US/training-courses/getting-started-with-template-10-16336?l=0FTjLmXRC_3905918567

---
+++ imoEstudante/ImoEstudantePL/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using ImoEstudantePL.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace ImoEstudantePL.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
