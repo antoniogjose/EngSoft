390cc63468e38e7610e9c48bdaaabb87258399fa
antoniogjose <antonio.g.joe@gmail.com>
2016-10-11 21:11:01 +0100

    Metro UI
    Inicio da construção do projeto em metro UI

--- imoEstudante/ImoEstudantePLMetroUi/Form1.Designer.cs
+++ imoEstudante/ImoEstudantePLMetroUi/Form1.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.menuToolStripMenuItem5,
+            this.menuToolStripMenuItem6,
+            this.clientesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(23, 60);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(802, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem1,
+            this.menuToolStripMenuItem2,
+            this.menuToolStripMenuItem3,
+            this.menuToolStripMenuItem4});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.menuToolStripMenuItem.Text = "File";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // menuToolStripMenuItem1
+            // 
+            this.menuToolStripMenuItem1.Name = "menuToolStripMenuItem1";
+            this.menuToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.menuToolStripMenuItem1.Text = "Menu";
+            // 
+            // menuToolStripMenuItem2
+            // 
+            this.menuToolStripMenuItem2.Name = "menuToolStripMenuItem2";
+            this.menuToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.menuToolStripMenuItem2.Text = "Menu";
+            // 
+            // menuToolStripMenuItem3
+            // 
+            this.menuToolStripMenuItem3.Name = "menuToolStripMenuItem3";
+            this.menuToolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.menuToolStripMenuItem3.Text = "Menu";
+            // 
+            // menuToolStripMenuItem4
+            // 
+            this.menuToolStripMenuItem4.Name = "menuToolStripMenuItem4";
+            this.menuToolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.menuToolStripMenuItem4.Text = "Menu";
+            // 
+            // menuToolStripMenuItem5
+            // 
+            this.menuToolStripMenuItem5.Name = "menuToolStripMenuItem5";
+            this.menuToolStripMenuItem5.Size = new System.Drawing.Size(72, 20);
+            this.menuToolStripMenuItem5.Text = "Alugueres";
+            // 
+            // menuToolStripMenuItem6
+            // 
+            this.menuToolStripMenuItem6.Name = "menuToolStripMenuItem6";
+            this.menuToolStripMenuItem6.Size = new System.Drawing.Size(78, 20);
+            this.menuToolStripMenuItem6.Text = "Habitações";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clientesToolStripMenuItem.Text = "Clientes";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 292);
+            this.ClientSize = new System.Drawing.Size(848, 396);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(23, 60, 23, 20);
+            this.Text = "ImoEstudante";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
     }
 }
 
--- imoEstudante/ImoEstudantePLMetroUi/Form1.cs
+++ imoEstudante/ImoEstudantePLMetroUi/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace ImoEstudantePLMetroUi
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroFramework.Forms.MetroForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
