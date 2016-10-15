a3d80d3df570f730a97c9ed2613c26898ae54088
antoniogjose <antonio.g.joe@gmail.com>
2016-10-15 18:47:53 +0100

    Janela de Login e inicio do layout

---
+++ imoEstudante/ImoEstudantePLMetroUi/Dasboard.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ImoEstudantePLMetroUi
+{
+    partial class Dasboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroTile5 = new MetroFramework.Controls.MetroTile();
+            this.metroTile6 = new MetroFramework.Controls.MetroTile();
+            this.metroTile7 = new MetroFramework.Controls.MetroTile();
+            this.metroTile8 = new MetroFramework.Controls.MetroTile();
+            this.pieChart2 = new LiveCharts.WinForms.PieChart();
+            this.pieChart3 = new LiveCharts.WinForms.PieChart();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.Location = new System.Drawing.Point(48, 42);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(123, 73);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "metroTile1";
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.Location = new System.Drawing.Point(177, 42);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(123, 73);
+            this.metroTile2.TabIndex = 0;
+            this.metroTile2.Text = "metroTile1";
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.Location = new System.Drawing.Point(48, 121);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(123, 73);
+            this.metroTile3.TabIndex = 0;
+            this.metroTile3.Text = "metroTile1";
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.Location = new System.Drawing.Point(177, 121);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(123, 73);
+            this.metroTile4.TabIndex = 0;
+            this.metroTile4.Text = "metroTile1";
+            // 
+            // metroTile5
+            // 
+            this.metroTile5.Location = new System.Drawing.Point(48, 270);
+            this.metroTile5.Name = "metroTile5";
+            this.metroTile5.Size = new System.Drawing.Size(123, 73);
+            this.metroTile5.TabIndex = 0;
+            this.metroTile5.Text = "metroTile1";
+            // 
+            // metroTile6
+            // 
+            this.metroTile6.Location = new System.Drawing.Point(177, 270);
+            this.metroTile6.Name = "metroTile6";
+            this.metroTile6.Size = new System.Drawing.Size(123, 73);
+            this.metroTile6.TabIndex = 0;
+            this.metroTile6.Text = "metroTile1";
+            // 
+            // metroTile7
+            // 
+            this.metroTile7.Location = new System.Drawing.Point(48, 349);
+            this.metroTile7.Name = "metroTile7";
+            this.metroTile7.Size = new System.Drawing.Size(123, 73);
+            this.metroTile7.TabIndex = 0;
+            this.metroTile7.Text = "metroTile1";
+            // 
+            // metroTile8
+            // 
+            this.metroTile8.Location = new System.Drawing.Point(177, 349);
+            this.metroTile8.Name = "metroTile8";
+            this.metroTile8.Size = new System.Drawing.Size(123, 73);
+            this.metroTile8.TabIndex = 0;
+            this.metroTile8.Text = "metroTile1";
+            // 
+            // pieChart2
+            // 
+            this.pieChart2.Location = new System.Drawing.Point(626, 322);
+            this.pieChart2.Name = "pieChart2";
+            this.pieChart2.Size = new System.Drawing.Size(200, 100);
+            this.pieChart2.TabIndex = 2;
+            this.pieChart2.Text = "pieChart2";
+            // 
+            // pieChart3
+            // 
+            this.pieChart3.Location = new System.Drawing.Point(362, 322);
+            this.pieChart3.Name = "pieChart3";
+            this.pieChart3.Size = new System.Drawing.Size(200, 100);
+            this.pieChart3.TabIndex = 3;
+            this.pieChart3.Text = "pieChart3";
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(362, 77);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(464, 220);
+            this.cartesianChart1.TabIndex = 4;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // Dasboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.pieChart3);
+            this.Controls.Add(this.pieChart2);
+            this.Controls.Add(this.metroTile8);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile7);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile6);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroTile5);
+            this.Controls.Add(this.metroTile1);
+            this.Name = "Dasboard";
+            this.Size = new System.Drawing.Size(854, 522);
+            this.Load += new System.EventHandler(this.Dasboard_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private MetroFramework.Controls.MetroTile metroTile5;
+        private MetroFramework.Controls.MetroTile metroTile6;
+        private MetroFramework.Controls.MetroTile metroTile7;
+        private MetroFramework.Controls.MetroTile metroTile8;
+        private LiveCharts.WinForms.PieChart pieChart2;
+        private LiveCharts.WinForms.PieChart pieChart3;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+    }
+}
---
+++ imoEstudante/ImoEstudantePLMetroUi/Dasboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImoEstudantePLMetroUi
+{
+    public partial class Dasboard : MetroFramework.Controls.MetroUserControl
+    {
+        public Dasboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dasboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
