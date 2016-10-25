f0b9e5749819c02d18aa9286291f6fb65d86c94c
antoniogjose <antonio.g.joe@gmail.com>
2016-10-25 15:52:29 +0100

    Função GMap (Comcluido)
    http://www.independent-software.com/gmap-net-beginners-tutorial-maps-markers-polygons-routes-updated-for-visual-studio-2015-and-gmap-net-1-7/
    http://stackoverflow.com/questions/10547147/a-working-gmap-net-application-source

--- imoEstudante/ImoEstudantePLMetroUi/AddHouse.Designer.cs
+++ imoEstudante/ImoEstudantePLMetroUi/AddHouse.Designer.cs
@@ -35,9 +35,11 @@
             this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
             this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
             this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
             this.panelDashBoard.SuspendLayout();
             this.panel1.SuspendLayout();
             this.metroTabControl1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelDashBoard
@@ -86,12 +88,12 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.metroTabControl1.Controls.Add(this.metroTabPage1);
-            this.metroTabControl1.Controls.Add(this.metroTabPage2);
             this.metroTabControl1.Controls.Add(this.metroTabPage3);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
             this.metroTabControl1.FontSize = MetroFramework.MetroTabControlSize.Small;
             this.metroTabControl1.Location = new System.Drawing.Point(0, 0);
             this.metroTabControl1.Name = "metroTabControl1";
-            this.metroTabControl1.SelectedIndex = 2;
+            this.metroTabControl1.SelectedIndex = 1;
             this.metroTabControl1.Size = new System.Drawing.Size(754, 449);
             this.metroTabControl1.TabIndex = 1;
             // 
@@ -108,6 +110,7 @@
             // 
             // metroTabPage2
             // 
+            this.metroTabPage2.Controls.Add(this.gMapControl1);
             this.metroTabPage2.HorizontalScrollbarBarColor = true;
             this.metroTabPage2.Location = new System.Drawing.Point(4, 35);
             this.metroTabPage2.Name = "metroTabPage2";
@@ -115,6 +118,7 @@
             this.metroTabPage2.TabIndex = 1;
             this.metroTabPage2.Text = "Mapa e Pontos de Interesse";
             this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.Enter += new System.EventHandler(this.metroTabPage2_Enter);
             // 
             // metroTabPage3
             // 
@@ -125,6 +129,34 @@
             this.metroTabPage3.TabIndex = 2;
             this.metroTabPage3.Text = "Avaliações";
             this.metroTabPage3.VerticalScrollbarBarColor = true;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 18);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(746, 389);
+            this.gMapControl1.TabIndex = 2;
+            this.gMapControl1.Zoom = 0D;
             // 
             // AddHouse
             // 
@@ -137,6 +169,7 @@
             this.panelDashBoard.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -150,5 +183,6 @@
         private MetroFramework.Controls.MetroTabPage metroTabPage1;
         private MetroFramework.Controls.MetroTabPage metroTabPage2;
         private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
     }
 }
--- imoEstudante/ImoEstudantePLMetroUi/AddHouse.cs
+++ imoEstudante/ImoEstudantePLMetroUi/AddHouse.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using GMap.NET.MapProviders;
+using GMap.NET;
+
 namespace ImoEstudantePLMetroUi
 {
     public partial class AddHouse : MetroFramework.Controls.MetroUserControl
@@ -26,6 +29,25 @@ namespace ImoEstudantePLMetroUi
         public AddHouse()
         {
             InitializeComponent();
+        }
+
+        private void metroTabPage2_Enter(object sender, EventArgs e)
+        {
+            //use google provider
+            gMapControl1.MapProvider = GoogleMapProvider.Instance;
+            //get tiles from server only
+            gMapControl1.Manager.Mode = AccessMode.ServerOnly;
+            //not use proxy
+            GMapProvider.WebProxy = null;
+            //center map on moscow
+            // gMapControl1.Position = new PointLatLng(55.755786121111, 37.617633343333);
+            gMapControl1.SetPositionByKeywords("Paris, France");
+
+            //zoom min/max; default both = 2
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 20;
+            //set zoom
+            gMapControl1.Zoom = 15;
         }
     }
 }
