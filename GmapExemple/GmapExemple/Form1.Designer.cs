46df7e5c23d01846be19cc1b3cd76bceb8d90513
antoniogjose <antonio.g.joe@gmail.com>
2016-11-14 15:24:58 +0000

    GMAP e POI´s testes
    Para posterior integração no projeto final

---
+++ GmapExemple/GmapExemple/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿using GMap.NET.WindowsForms;
+
+namespace GmapExemple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tBLgn = new System.Windows.Forms.TextBox();
+            this.tBLat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lV_Poi = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 18;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(785, 371);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+            this.gMapControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tBLgn);
+            this.panel1.Controls.Add(this.tBLat);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(183, 52);
+            this.panel1.TabIndex = 1;
+            // 
+            // tBLgn
+            // 
+            this.tBLgn.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBLgn.Location = new System.Drawing.Point(38, 31);
+            this.tBLgn.Name = "tBLgn";
+            this.tBLgn.Size = new System.Drawing.Size(100, 13);
+            this.tBLgn.TabIndex = 1;
+            // 
+            // tBLat
+            // 
+            this.tBLat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBLat.Location = new System.Drawing.Point(38, 4);
+            this.tBLat.Name = "tBLat";
+            this.tBLat.Size = new System.Drawing.Size(100, 13);
+            this.tBLat.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lgn :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lat :";
+            // 
+            // lV_Poi
+            // 
+            this.lV_Poi.Location = new System.Drawing.Point(12, 76);
+            this.lV_Poi.Name = "lV_Poi";
+            this.lV_Poi.Size = new System.Drawing.Size(183, 307);
+            this.lV_Poi.TabIndex = 2;
+            this.lV_Poi.UseCompatibleStateImageBehavior = false;
+            this.lV_Poi.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 395);
+            this.Controls.Add(this.lV_Poi);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBLgn;
+        private System.Windows.Forms.TextBox tBLat;
+        private GMapControl gMapControl1;
+        private System.Windows.Forms.ListView lV_Poi;
+    }
+}
+
---
+++ GmapExemple/GmapExemple/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using GMap.NET.WindowsForms.ToolTips;
+
+using System.Globalization;
+using System.Net;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace GmapExemple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible = false;
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+
+            //use google provider
+            gMapControl1.MapProvider = GoogleMapProvider.Instance;
+            //get tiles from server only
+            gMapControl1.Manager.Mode = AccessMode.ServerOnly;
+            //not use proxy
+            GMapProvider.WebProxy = null;
+            //center map on moscow
+            // gMapControl1.Position = new PointLatLng(55.755786121111, 37.617633343333);
+            gMapControl1.SetPositionByKeywords("Barcelos, Portugal");
+
+            gMapControl1.DragButton = MouseButtons.Left;
+
+
+            //zoom min/max; default both = 2
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 20;
+            //set zoom
+            gMapControl1.Zoom = 15;
+
+            /**
+                        // Add a Marquer
+                        GMapOverlay markers = new GMapOverlay("markers");
+                        GMapMarker marker = new GMarkerGoogle(
+                            new PointLatLng(48.8617774, 2.349272),
+                            GMarkerGoogleType.blue_pushpin);
+                        markers.Markers.Add(marker);
+                        gMapControl1.Overlays.Add(markers);**/
+            GMapOverlay markers = new GMapOverlay("markers");
+            GMapMarker marker = new GMarkerGoogle(new PointLatLng(41.5361453446439, -8.6068868637085), GMarkerGoogleType.blue_pushpin);
+            markers.Markers.Add(marker);
+            gMapControl1.Overlays.Add(markers);
+
+
+        }
+
+        private void gMapControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                panel1.Visible = true;
+                double latfinal;
+                double lngfinal;
+                double lat = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lat;
+                double lng = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lng;
+
+                lV_Poi.Clear();
+
+                /*
+                NumberStyles style = NumberStyles.Number | NumberStyles.AllowCurrencySymbol;
+                CultureInfo culture = CultureInfo.CreateSpecificCulture("en-GB");
+                    Decimal.TryParse(l, style, culture, out latfinal);
+                    Decimal.TryParse(g, style, culture, out lngfinal);
+                 
+                 */
+
+                tBLat.Text = lat.ToString();//.Replace(',', '.');
+                tBLgn.Text = lng.ToString();//.Replace(',', '.');
+                string l = lat.ToString().Replace(',', '.');
+                string g = lng.ToString().Replace(',', '.');
+                NumberStyles style = NumberStyles.Number | NumberStyles.AllowCurrencySymbol;
+                CultureInfo culture = CultureInfo.CreateSpecificCulture("en-GB");
+                Double.TryParse(l, style, culture, out latfinal);
+                Double.TryParse(g, style, culture, out lngfinal);
+
+                // Add a Marquer
+              //  GMapOverlay markers = new GMapOverlay("markers");
+              //  GMapMarker marker = new GMarkerGoogle( new PointLatLng(latfinal, lngfinal), GMarkerGoogleType.red_big_stop);
+              //  markers.Markers.Add(marker);
+              //  gMapControl1.Overlays.Add(markers);
+
+                // Adicionar POI´s
+
+                WebClient webClient = new WebClient();
+
+                string uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+                uri += "location=" + l + "," + g + "&";
+                uri += "radius=" + "500" + "&";
+                uri += "types=point_of_interest&";
+                uri += "key=AIzaSyAk7AXTSD8FhpqAixnP6_Alh3uOh4v1198";
+
+
+                string results = webClient.DownloadString(uri);
+
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+
+                Example pois = jss.Deserialize<Example>(results);
+
+
+                for (int i = 0; i < pois.results.Count; i++)
+                {
+                    // apresentar o nome do POI em portugues
+                    byte[] bytes = Encoding.Default.GetBytes(pois.results[i].name);
+                    string myString = Encoding.UTF8.GetString(bytes);
+                    lV_Poi.Items.Add(myString);
+                }
+
+            }
+        }
+    }
+}
